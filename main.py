62656b2cbe7901ee019f5b6739f76868ea2d3219
gr1ll0o <thiagotobias.grillo@gmail.com>
2025-08-12 19:52:00 -0300

    Removed libs unused

--- main.py
+++ main.py
@@ -8,8 +8,6 @@ from email.mime.text import MIMEText
 from email.mime.image import MIMEImage
 from email.mime.multipart import MIMEMultipart
 from email.mime.application import MIMEApplication
-from email.mime.base import MIMEBase
-from email import encoders
 import threading
 import time
 import os
